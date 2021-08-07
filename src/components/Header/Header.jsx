dc962ace151ebc886ab8133db8758d357ba20d97
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-08-08 01:00:18 +0530

    lerning-apis

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = function () {
+  return <>Header</>;
+};
+
+export default Header;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const List = function () {
+  return <>List</>;
+};
+
+export default List;
